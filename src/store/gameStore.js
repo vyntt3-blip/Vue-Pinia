f1893a89c6c3d240de0451c719c94bc95bb8b3a8
vynguyentran <vyntt3@runsystem.net>
2026-01-30 16:28:30 +0700

    feat(pinia-composition-api): using composition api style

--- src/store/gameStore.js
+++ src/store/gameStore.js
@@ -1,47 +1,93 @@
 import { defineStore } from "pinia";
+import { ref, computed } from "vue";
 
-export const useGameStore = defineStore("gameStore", {
-  state: () => ({
-    score: 0,
-    maxHeath: 100,
-    maxAttack: 30,
-    maxDefense: 10,
-  }),
-  getters: {
-    getScore() {
-      return this.score;
-    },
-    getWinningsGame() {
-      return this.maxHeath;
-    }
-    // // voi actions in pinia doi get thanh set
-    // setNextAttack() {
-    //   let attack = Math.floor(Math.random() * this.maxAttack) + 1;
-    //   console.log("attach: ", attack);
-    //   return this.score + attack;
-    // },
-    // setNextDefense() {
-    //   let defense = Math.floor(Math.random() * this.maxDefense);
-    //   console.log("defense: ", defense);
-    //   return this.score - defense;
-    // },
-  },
-  actions: {
-    // voi actions in pinia doi get thanh set
-    setNextAttack() {
-      let attack = Math.floor(Math.random() * this.maxAttack) + 1;
-      console.log("attach: ", attack);
-      // return this.score + attack;
-      this.score += attack;
-    },
-    setNextDefense() {
-      let defense = Math.floor(Math.random() * this.maxDefense);
-      console.log("defense: ", defense);
-      // return this.score - defense;
-      this.score -= defense;
-    },
-    resetGame() {
-      this.score = 0;
-    },
+export const useGameStore = defineStore("gameStore", () => {
+  // State
+  const score = ref(0);
+  const maxHeath = ref(100);
+  const maxAttack = ref(30);
+  const maxDefense = ref(10);
+
+  // Getters
+  const getScore = computed(() => score.value);
+  const getWinningsScore = computed(() => maxHeath.value);
+
+  function setNextAttack() {
+    const attack = Math.floor(Math.random() * maxAttack.value) + 1;
+    console.log("attack: ", attack);
+    score.value += attack;
   }
+
+  function setNextDefense() {
+    const defense = Math.floor(Math.random() * maxDefense.value) + 1;
+    console.log("defense: ", defense);
+    score.value -= defense;
+  }
+
+  function resetGame() {
+    score.value = 0;
+  }
+
+  return {
+    // State
+    score,
+    maxAttack,
+    maxHeath,
+    maxDefense,
+    
+    // Getters
+    getScore,
+    getWinningsScore,
+
+    // Actions
+    setNextAttack,
+    setNextDefense,
+    resetGame,
+  };
 });
+
+
+// import { defineStore } from "pinia";
+// import { ref, computed } from "vue";
+
+// export const useGameStore = defineStore("gameStore", () => {
+//   const score = ref(0);
+//   const maxHeath = ref(100);
+//   const maxAttack = ref(30);
+//   const maxDefense = ref(10);
+
+//   const getScore = computed(() => score.value);
+//   const getWinningsScore = computed(() => maxHeath.value);
+
+//   setNextAttack = () => {
+//     let attack = Math.floor(Math.random() * maxAttack.value) + 1;
+//     console.log("attach: ", attack);
+//     // return this.score + attack;
+//     score.value += attack;
+//   };
+//   setNextDefense = () => {
+//     let defense = Math.floor(Math.random() * maxDefense.value);
+//     console.log("defense: ", defense);
+//     // return this.score - defense;
+//     score.value -= defense;
+//   };
+//   resetGame = () => {
+//     score.value = 0;
+//   };
+
+//   return {
+//     //state
+//     score,
+//     maxAttack,
+//     maxHeath,
+//     maxDefense,
+//     //getters
+//     getScore,
+//     getWinningsScore,
+
+//     //actions
+//     setNextAttack,
+//     setNextDefense,
+//     resetGame,
+//   };
+// });
